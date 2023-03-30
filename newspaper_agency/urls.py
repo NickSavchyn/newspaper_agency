82c51e528b7a5d665a5eac520e75b9c7e8353b5b
Mykola Savchyn <kola.savchyn@gmail.com>
2023-03-30 23:45:28 +0300

    Add list htmlsl

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -3,17 +3,17 @@ from django.urls import path
 
 from .views import (
     index,
-#     NewspaperListView,
+    NewspaperListView,
 #     NewspaperDetailView,
 #     NewspaperCreateView,
 #     NewspaperUpdateView,
 #     NewspaperDeleteView,
-#     RedactorListView,
+     RedactorListView,
 #     RedactorDetailView,
 #     RedactorCreateView,
 #     RedactorUpdateView,
 #     RedactorDeleteView,
-#     TopicListView,
+    TopicListView,
 #     TopicCreateView,
 #     TopicUpdateView,
 #     TopicDeleteView,
@@ -23,11 +23,11 @@ urlpatterns = [
     path(
         "", index, name="index"
     ),
-#     path(
-#         "topics/",
-#         TopicListView.as_view(),
-#         name="topic-list",
-#     ),
+    path(
+        "topics/",
+        TopicListView.as_view(),
+        name="topic-list",
+    ),
 #     path(
 #         "topics/create/",
 #         TopicCreateView.as_view(),
@@ -43,9 +43,9 @@ urlpatterns = [
 #         TopicDeleteView.as_view(),
 #         name="topic-delete",
 #     ),
-#     path(
-#         "newspapers/", NewspaperListView.as_view(), name="newspaper-list"
-#     ),
+    path(
+         "newspapers/", NewspaperListView.as_view(), name="newspaper-list"
+    ),
 #     path(
 #         "newspapers/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"
 #     ),
@@ -64,9 +64,9 @@ urlpatterns = [
 #     path(
 #         "redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"
 #     ),
-#     path(
-#         "redactors/", RedactorListView.as_view(), name="redactor-list"
-#     ),
+     path(
+         "redactors/", RedactorListView.as_view(), name="redactor-list"
+     ),
 #     path(
 #         "redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"
 #     ),
--- newspaper_agency/views.py
+++ newspaper_agency/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
@@ -28,3 +27,23 @@ def index(request):
     }
 
     return render(request, "newspaper_agency/index.html", context=context)
+
+
+class TopicListView(LoginRequiredMixin, generic.ListView):
+    model = Topic
+    context_object_name = "topic_list"
+    template_name = "newspaper_agency/topic_list.html"
+    paginate_by = 5
+
+
+class NewspaperListView(LoginRequiredMixin, generic.ListView):
+    model = Newspaper
+    paginate_by = 5
+    template_name = "newspaper_agency/newspaper_list.html"
+    queryset = Newspaper.objects.all().select_related("topic")
+
+
+class RedactorListView(LoginRequiredMixin, generic.ListView):
+    model = Redactor
+    paginate_by = 5
+    template_name = "newspaper_agency/redactor_list.html"
