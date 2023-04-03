3748055f5a607a5209c34515205d07227201e99a
Mykola Savchyn <kola.savchyn@gmail.com>
2023-04-03 18:04:25 +0300

    Connect PostgreSQL db

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 from django.conf.urls import static
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
@@ -89,6 +91,9 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 
 
 # Password validation
